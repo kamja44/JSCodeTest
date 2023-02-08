8fb0a4091d789963192ae95462747b8e83181dad
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-02-08 13:18:46 +0900

    11 ~ 20

---
+++ 11~20/16.js
@@ -0,0 +1,6 @@
+/*
+문장이 입력되면 거꾸로 출력하는 프로그램을 만들어 봅시다.
+*/
+const input = prompt("입력");
+
+console.log(input.split("").reverse().join(""));
---
+++ 11~20/19.js
@@ -0,0 +1,5 @@
+/*
+공백으로 구분하여 두 숫자 a와 b가 주어지면, **a의 b승**을 구하는 프로그램을 작성하세요.
+*/
+const input = prompt("공백으로 두 숫자 입력").split(" ");
+console.log(input[0] ** input[1]);
